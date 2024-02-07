dbe1037d3b0a6d75324a81a31e3668836c356879
Paramjitsaikia001 <paramjitsaikia@gmail.com>
2024-02-07 22:37:03 +0530

    DAY=02

---
+++ string.py
@@ -0,0 +1,7 @@
+# three different type of representation of string
+str1="My name is Paramjit Saikia.\n My father name is Padma Saikia\n"
+str2='i live in guwahati.\t i pursue bsc cs\n'
+str3="""My age is 18"""
+print(str1,str2,str3)
+print(str1+str2)
+print(len(str3))
