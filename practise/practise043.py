f3c166e71ff5a4abeced7979908cfbaf0e37b4fc
Paramjit Saikia <paramjitsaikia@gmail.com>
2025-12-29 23:30:13 +0530

    implement checkInclusion method to determine if one string's permutation is in another

---
+++ practise/practise043.py
@@ -0,0 +1,25 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1)>len(s2):
+            return False
+        count1=[0]*26
+        count2=[0]*26
+
+        for i in range(len(s1)):
+            count1[ord(s1[i])-97]+=1
+            count2[ord(s2[i])-97]+=1
+        if count1==count2:
+            return True
+        j=0
+        for i in range(len(s1),len(s2)):
+            count2[ord(s2[i])-97]+=1
+            count2[ord(s2[j])-97]-=1
+            if count1==count2:
+                return True
+            j+=1
+        return False
+        
+    
+s=Solution()
+print(s.checkInclusion("ab","eidboaoo"))
+            
